91858032f3ee30e43bc465f3e19125b132197a32
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-10-04 15:22:29 +0600

    vedio time and authors name write done.

---
+++ JS/varified.js
@@ -0,0 +1,14 @@
+// It's just bojar jonno----->
+
+// const isVerified = true;
+// const isVerified = false;
+
+// if (isVerified === true) {
+//   console.log("User is verified.");
+// } else {
+//   console.log("User is not verified.");
+// }
+
+// console.log(
+//   `${isVerified === true ? "User is verified." : "user is not verified"}`
+// );
--- JS/vedio.js
+++ JS/vedio.js
@@ -67,22 +67,37 @@ const displayVideos = (videos) => {
     const card = document.createElement("div");
     card.classList = "card card-compact";
     card.innerHTML = `
-    <figure class ="h-[200px]">
+    <figure class ="h-[200px] relative">
     <img
       class = "h-full w-full object-cover"
       src= ${video.thumbnail} />
+
+      ${
+        video.others.posted_date?.length == 0
+          ? ""
+          : `<span class = "absolute right-2 bottom-2 text-white bg-black rounded p-1"> ${video.others.posted_date} </span>`
+      }
+      
   </figure>
+
   <div class="px-0 py-2">
     
     <div>
-        <img class ="w-10 h-10 rounded-full object-cover" src= "${video.authors[0].profile_picture}" />
+        <img class ="w-10 h-10 rounded-full object-cover" src= "${
+          video.authors[0].profile_picture
+        }" />
     </div>
 
     <div>
         <h2 class = "font-bold"> ${video.title} </h2>
         <div class ="flex items-center gap-2">
            <p class = "text-gray-400"> ${video.authors[0].profile_name} </P>
-           <img class = "w-5" src = "https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />
+           
+           ${
+             video.authors[0].verified === true
+               ? '<img class = "w-5" src = "https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />'
+               : ""
+           }
         </div>
 
         <p> </P>
@@ -95,3 +110,7 @@ const displayVideos = (videos) => {
 
 loadCategories();
 loadVideos();
+
+{
+  /*  */
+}
